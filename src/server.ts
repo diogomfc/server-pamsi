75b78b0c0845523bb146d71086cbcab5b5820f65
Diogo Silva <diogomfc@hotmail.com>
2023-10-23 10:38:58 -0300

    Configuração do middleware de tratamento de erros: Implementado o middleware para lidar com erros da aplicação e erros internos do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import 'express-async-errors';
 
-import express from 'express';
+import express, { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 
 import { env } from './env';
 import { AppError } from './utils/AppError';
@@ -10,6 +10,22 @@ const app = express();
 
 
 app.use(express.json());
+
+app.use((err: ErrorRequestHandler, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+    next();
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen({
     port: env.PORT,
